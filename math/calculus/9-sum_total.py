db6a125f9159436130475829f35e4ed0eca010fe
PoP-MALIK <melek.jaffel@gmail.com>
2026-07-07 15:20:43 +0000

    m

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -4,8 +4,6 @@
 
 def summation_i_squared(n):
     """calculates the sum of i squared from 1 to n """
-
-    if  type(n) is not int or n < 1:
-        return None 
-
+    if type(n) is not int or n < 1:
+        return None
     return (n * (n + 1) * (2 * n + 1)) // 6
