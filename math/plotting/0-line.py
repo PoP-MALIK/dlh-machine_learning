3915ddfac17a138600469378304373e1d7042a17
PoP-MALIK <melek.jaffel@gmail.com>
2026-07-13 20:51:57 +0200

    fix readme

--- math/plotting/0-line.py
+++ math/plotting/0-line.py
@@ -7,12 +7,9 @@ import matplotlib.pyplot as plt
 
 def line():
     """Plot y as a solid red line graph."""
-
     y = np.arange(0, 11) ** 3
     plt.figure(figsize=(6.4, 4.8))
 
     x = np.arange(0, 11)
     plt.plot(x, y, 'r-')
     plt.xlim(0, 10)
-
-    plt.show()
