eab43c17c6aa64969e7843f5a4f9ab7c354547e9
PoP-MALIK <melek.jaffel@gmail.com>
2026-08-09 19:53:53 +0200

    m

--- math/probability/normal.py
+++ math/probability/normal.py
@@ -57,4 +57,31 @@ class Normal:
         Returns:
             float: x value of z
         """
-        return (z * self.stddev) + self.mean
+        return (z * self.stddev) + self.mea
+
+    class Normal:
+    """Represents a normal distribution."""
+
+    def __init__(self, data=None, mean=0., stddev=1.):
+        """Initialize Normal with either data or mean/stddev."""
+        if data is None:
+            if stddev <= 0:
+                raise ValueError('stddev must be a positive value')
+            self.mean = float(mean)
+            self.stddev = float(stddev)
+        else:
+            if not isinstance(data, list):
+                raise TypeError('data must be a list')
+            if len(data) < 2:
+                raise ValueError('data must contain multiple values')
+            self.mean = float(sum(data) / len(data))
+            variance = sum((x - self.mean) ** 2 for x in data) / len(data)
+            self.stddev = float(variance ** 0.5)
+
+    def z_score(self, x):
+        """Calculates the z-score of a given x value."""
+        return (x - self.mean) / self.stddev
+
+    def x_value(self, z):
+        """Calculates the x value of a given z-score."""
+        return (z * self.stddev) + self.meann
